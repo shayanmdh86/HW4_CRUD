cb15a6c39abb5a87da8b7b578ab507cf41aae800
shayanmdh86 <shayanmdhy0@gmail.com>
2023-05-27 18:26:34 +0330

    Add project files.

---
+++ HW4/Entity/User.cs
@@ -0,0 +1,41 @@
+﻿namespace HW4.Entity
+{
+    public class User
+    {
+        public int Id { get { return _id; } }
+        private int _id { get; set; }
+
+        public string Name { get { return _name; } }
+        private string _name { get; set; }
+
+        public string Mobile { get { return _mobile; } }
+        private string _mobile { get; set; }
+
+        public DateTime birthDate { get { return _birthdate; } }
+        private DateTime _birthdate { get; set; }
+
+        public DateTime createDate { get { return _createDate; } }
+        private DateTime _createDate { get; set; }
+
+        public User(int id, string name, string mobile, DateTime birthdate, DateTime createDate)
+        {
+            _id = id;
+            _name = name;
+
+            if (mobile != null || mobile.Length != 11)
+            {
+                Console.WriteLine("Your number is correct!! ");
+            }
+            else
+            {
+                _mobile = mobile;
+
+            }
+
+
+
+            _birthdate = birthdate;
+            _createDate = createDate;
+        }
+    }
+}
